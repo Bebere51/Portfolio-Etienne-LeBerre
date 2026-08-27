ff430d3c5ae3fcea51b3254c4fac78bb6d12ec8f
Bebere51 <100358506+Bebere51@users.noreply.github.com>
2026-08-27 23:24:53 +0200

    Create consts.ts for site constants
    Add sitewide constants for site name and description.

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+/**
+ * Any sort of sitewide constants should be kept here following the same
+ * naming convention of all caps snakecase
+ *
+ * e.g.
+ *
+ * export const YEAR_ESTABLISHED = 2020
+ */
+
+export const SITE_NAME = 'Étienne Le Berre';
+export const SITE_DESCRIPTION = 'Portfolio personnel — remplace cette phrase par ta propre description.';
